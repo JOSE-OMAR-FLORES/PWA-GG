77b17699292dbff198ae35d7a875d95ef10a5567
JOSE-OMAR-FLORES <153056143+JOSE-OMAR-FLORES@users.noreply.github.com>
2025-09-18 22:02:14 -0600

    feat: agregar husky, commitlint y CI/CD con github actions

---
+++ workbox-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	globDirectory: 'dist/',
+	globPatterns: [
+		'**/*.{js,css,svg,html}'
+	],
+	swDest: 'dist/sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/,
+		/^utm_/,
+		/^fbclid/
+	]
+};
